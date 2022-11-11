70705c7eb2aec50ed4e66a8f34ed58cd6c49de54
Siena <siena.cizdziel@yale.edu>
2022-11-10 21:53:01 -0500

    curtain and star logic

--- ESP32/clouds_and_multithreading/clouds_and_multithreading.ino
+++ ESP32/clouds_and_multithreading/clouds_and_multithreading.ino
@@ -11,22 +11,34 @@ const int stepsPerRevolution = 2048;
 #define IN4 17
 #define CLOUD_PIN 26 
 #define CURTAIN_PIN_1 22
+#define CURTAIN_PIN_2 21
+#define BUTTON 14
 
+// initialize motors
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 Servo cloudMotor;
 Servo curtainMotor1;
+Servo curtainMotor2;
+
+// variables for movement
 int distance = 50;
 int speed = 5;
 int cloudPos = 0;
 int curtainPos1 = 0;
 int mapSpeed;
 
+// states
+int on = 0;
+
+// multithreading
 TaskHandle_t clouds;
 
 void setup() {
   myStepper.setSpeed(5);
+  pinMode(BUTTON, INPUT);
   cloudMotor.attach(CLOUD_PIN);
   curtainMotor1.attach(CURTAIN_PIN_1);
+  curtainMotor2.attach(CURTAIN_PIN_2);
   Serial.begin(115200);
 
   // multithreading for task 1 (rotating servo)
@@ -40,21 +52,43 @@ void setup() {
       &clouds,  /* Task handle. */
       0); /* Core where the task should run */
 
-      Serial.println(xPortGetCoreID());
-
-  // for (int i = 0; i < 360; i += 1) {
-  //   curtainMotor1.write(i);
-  //   delay(10);
-  // }
+  // reset motors
   curtainMotor1.write(0);
+  curtainMotor2.write(0);
 }
 
 void loop() {
+  // read start button
+  int button = digitalRead(BUTTON);
+  if (button == LOW && on == 0) {
+    on = 1;
+  } else if (on == 1) {
+    long int start = millis();
+    // wait 3 seconds in night mode
+    while (millis() - start < 3000) {
+        myStepper.step(stepsPerRevolution);
+    }
+    on = 2;
+
+    // close curtains for day mode
+    curtainMotor1.write(180);
+  } 
+
+
   // step one revolution in one direction:
   // myStepper.step(stepsPerRevolution);
-  // Serial.println(xPortGetCoreID());
 
+  // Serial.println(state);
+  // while (state == "closed") {
   // curtain 1
+    // curtainMotor1.write(180);
+    // delay(1000);
+    // curtainMotor1.write(0);
+    // delay(1000);
+    // curtainMotor1.write()
+  // }
+
+  
 
   // moveTo(0, speed);
   // moveTo(50, speed);
@@ -81,8 +115,30 @@ void cloudMove(int position, int speed) {
 
 // clouds
 void cloudsFunction( void * parameter) {
-  for(;;) {
-    cloudMove(0, 5);
-    cloudMove(50, 5);
+  if (on == 1) {
+    long int start = millis();
+    while (millis() - start < 3000) {
+    //   Serial.println(millis(), start);
+      cloudMove(0, 5);
+      cloudMove(50, 5);
+    //   curtainMotor2.write(0);
+    //   delay(2000);
+    //   curtainMotor2.write(180);
+    //   delay(2000);
+    }
+
+    // curtain 2
+    // state = "closed";
+    curtainMotor2.write(180);
+    delay(2000);
+
+    for (;;) {
+      cloudMove(0, 5);
+      cloudMove(50, 5);   
+      // curtainMotor2.write(0);
+      // delay(2000);
+      // curtainMotor2.write(180);
+      // delay(2000);     
+    }
   }
 }
