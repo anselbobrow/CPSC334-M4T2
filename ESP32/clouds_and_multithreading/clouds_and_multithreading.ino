86b97937149b4317ae1147a6ecb6855a6bcd5612
Siena <siena.cizdziel@yale.edu>
2022-11-11 18:11:18 -0500

    fully finished code

--- ESP32/clouds_and_multithreading/clouds_and_multithreading.ino
+++ ESP32/clouds_and_multithreading/clouds_and_multithreading.ino
@@ -24,14 +24,18 @@ Servo curtainMotor2;
 int distance = 50;
 int speed = 5;
 int cloudPos = 0;
-int curtainPos1 = 0;
 int mapSpeed;
+unsigned long start;
+unsigned long curtain_movement; 
+unsigned long sun_moving;
+unsigned long offset;
 
 // states
 int on = 0;
 
 // multithreading
 TaskHandle_t clouds;
+TaskHandle_t stars;
 
 void setup() {
   myStepper.setSpeed(5);
@@ -41,58 +45,33 @@ void setup() {
   curtainMotor2.attach(CURTAIN_PIN_2);
   Serial.begin(115200);
 
-  // multithreading for task 1 (rotating servo)
   // https://randomnerdtutorials.com/esp32-dual-core-arduino-ide/ 
+    // multithreading for task 0 (rotating stepper)
   xTaskCreatePinnedToCore(
-      cloudsFunction, /* Function to implement the task */
+      stageLeft, /* Function to implement the task */
+      "stars", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      tskIDLE_PRIORITY,  /* Priority of the task */
+      &stars,  /* Task handle. */
+      0); /* Core where the task should run */
+
+  // multithreading for task 1 (rotating servo)
+  xTaskCreatePinnedToCore(
+      stageRight, /* Function to implement the task */
       "clouds", /* Name of the task */
       10000,  /* Stack size in words */
       NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
+      1,  /* Priority of the task */
       &clouds,  /* Task handle. */
-      0); /* Core where the task should run */
+      1); /* Core where the task should run */
 
   // reset motors
   curtainMotor1.write(0);
-  curtainMotor2.write(0);
+  curtainMotor2.write(180);
 }
 
-void loop() {
-  // read start button
-  int button = digitalRead(BUTTON);
-  if (button == LOW && on == 0) {
-    on = 1;
-  } else if (on == 1) {
-    long int start = millis();
-    // wait 3 seconds in night mode
-    while (millis() - start < 3000) {
-        myStepper.step(stepsPerRevolution);
-    }
-    on = 2;
-
-    // close curtains for day mode
-    curtainMotor1.write(180);
-  } 
-
-
-  // step one revolution in one direction:
-  // myStepper.step(stepsPerRevolution);
-
-  // Serial.println(state);
-  // while (state == "closed") {
-  // curtain 1
-    // curtainMotor1.write(180);
-    // delay(1000);
-    // curtainMotor1.write(0);
-    // delay(1000);
-    // curtainMotor1.write()
-  // }
-
-  
-
-  // moveTo(0, speed);
-  // moveTo(50, speed);
-}
+void loop() {} 
 
 // rotates servo to position at specified speed
 int pos1;
@@ -114,31 +93,80 @@ void cloudMove(int position, int speed) {
 }
 
 // clouds
-void cloudsFunction( void * parameter) {
-  if (on == 1) {
-    long int start = millis();
-    while (millis() - start < 3000) {
-    //   Serial.println(millis(), start);
+void stageRight(void * parameter) {
+  // includes stars and curtain motor 2
+    // curtain angles: open = 180, closed = 0
+  
+  int timing = 14000;
+  while (true) { 
+    int button = digitalRead(BUTTON);
+    if (button == HIGH && on == 0) {
+      on = 1;
+      start = millis();
+    } else if (on == 1) {
+      Serial.println(millis() - start);
+      if (millis() - start > 500) {
+        curtain_movement = millis();
+        on = 2; // moving curtains  
+      } else {
+        cloudMove(0, 5);
+        cloudMove(50, 5);
+      }
+    } else if (on == 2) {
+      if (millis() - curtain_movement > 2000) {
+        curtainMotor2.write(0); 
+        curtainMotor1.write(180); 
+        on = 3;
+        sun_moving = millis();
+      } else {
+        cloudMove(0, 5);
+        cloudMove(50, 5);
+      }
+    } else if (on == 3) {
       cloudMove(0, 5);
       cloudMove(50, 5);
-    //   curtainMotor2.write(0);
-    //   delay(2000);
-    //   curtainMotor2.write(180);
-    //   delay(2000);
+      if (millis() - sun_moving > timing) {
+        on = 4; 
+        curtain_movement = millis();
+        if (millis() - sun_moving > timing + 300) {
+          curtainMotor2.write(180);
+          curtainMotor1.write(0);
+        }
+      }
+    } else if (on == 4) {
+      if (millis() - curtain_movement > 3000) {
+        on = 5;
+        curtainMotor2.write(0);
+        curtainMotor1.write(180);
+        sun_moving = millis();
+      }
+      else {
+        cloudMove(0, 5);
+        cloudMove(50, 5);
+      }
+    } else if (on == 5) {
+      if (millis() - sun_moving > timing) {
+        on = 0;
+        curtainMotor2.write(180);
+        curtainMotor1.write(0);
+      } else {
+        cloudMove(0, 5);
+        cloudMove(50, 5);
+      }
     }
+  }
+}
 
-    // curtain 2
-    // state = "closed";
-    curtainMotor2.write(180);
-    delay(2000);
-
-    for (;;) {
-      cloudMove(0, 5);
-      cloudMove(50, 5);   
-      // curtainMotor2.write(0);
-      // delay(2000);
-      // curtainMotor2.write(180);
-      // delay(2000);     
+void stageLeft(void *parameter) {
+  // includes clouds and curtain motor 1
+    // curtain angles: open = 0, closed = 180
+  
+  int open = 1;
+  bool offset_bool = false;
+  while (true) {
+    Serial.println(on);
+    if (on == 1 || on == 2 || on == 3 || on == 4) {
+      myStepper.step(stepsPerRevolution);  
     }
   }
 }
